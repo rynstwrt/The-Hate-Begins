4cbf2cafe3af693dabe9125c786805eea0eb7f06
Ryan Stewart <ryanstewartalex@gmail.com>
2023-05-16 23:33:41 -0500

    Start Adding Ground

--- src/app.ts
+++ src/app.ts
@@ -4,32 +4,48 @@ import "@babylonjs/loaders/glTF";
 import {
     Engine,
     Scene,
-    ArcRotateCamera,
     Vector3,
     HemisphericLight,
-    Mesh,
     MeshBuilder,
-    UniversalCamera
+    UniversalCamera,
+    Color4,
+    StandardMaterial,
+    Color3,
+    Texture
 } from "@babylonjs/core";
 
 class App
 {
+    scene: Scene;
+    engine: Engine;
+
+    readonly backgroundSquaresX = 5;
+    readonly backgroundSquaresY = 5;
+    background: Element;
+
+    groundWidth = 100;
+    groundHeight = 100;
+
     constructor()
     {
+        this.background = document.getElementById("background");
+        this.createBackground(this.backgroundSquaresX, this.backgroundSquaresY);
+
         const canvas = document.createElement("canvas");
         canvas.style.width = "100%";
         canvas.style.height = "100%";
         canvas.id = "gameCanvas";
         document.body.appendChild(canvas);
 
-        const engine = new Engine(canvas, true);
+        this.engine = new Engine(canvas, true);
 
-        const scene = new Scene(engine);
-        scene.gravity = new Vector3(0, -.75, 0);
-        scene.collisionsEnabled = true;
-        scene.enablePhysics();
+        this.scene = new Scene(this.engine);
+        this.scene.clearColor = new Color4(0, 0, 0, 0);
+        this.scene.gravity = new Vector3(0, -.75, 0);
+        this.scene.collisionsEnabled = true;
+        this.scene.enablePhysics();
 
-        const camera: UniversalCamera = new UniversalCamera("camera", new Vector3(0, 3, -20), scene);
+        const camera: UniversalCamera = new UniversalCamera("camera", new Vector3(0, 3, -20), this.scene);
         camera.setTarget(Vector3.Zero());
         camera.applyGravity = true;
         camera.ellipsoid = new Vector3(1, 1.5, 1);
@@ -42,7 +58,7 @@ class App
         camera.keysLeft.push(65);
 
         let isLocked = false;
-        scene.onPointerDown = event =>
+        this.scene.onPointerDown = event =>
         {
             if (!isLocked)
             {
@@ -67,31 +83,72 @@ class App
         document.addEventListener("mozpointerlockchange", pointerLockChange, false);
         document.addEventListener("webkitpointerlockchange", pointerLockChange, false);
 
-        const myGround = MeshBuilder.CreateGround("myGround", { width: 200, height: 200}, scene);
-        myGround.checkCollisions = true;
-
-        const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-
-        // hide/show the Inspector
         window.addEventListener("keydown", (ev) =>
         {
             // Shift+Ctrl+Alt+I
             if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === 'i')
             {
-                if (scene.debugLayer.isVisible())
+                if (this.scene.debugLayer.isVisible())
                 {
-                    scene.debugLayer.hide();
+                    this.scene.debugLayer.hide();
                 } else
                 {
-                    scene.debugLayer.show();
+                    this.scene.debugLayer.show();
                 }
             }
         });
 
-        // run the main render loop
-        engine.runRenderLoop(() => {
-            scene.render();
+        window.addEventListener("resize", () =>
+        {
+            this.engine.resize();
+            this.createBackground(this.backgroundSquaresX, this.backgroundSquaresX);
         });
+
+        const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this.scene);
+        this.createGround();
+
+        this.engine.runRenderLoop(() =>
+        {
+            this.scene.render();
+        });
+    }
+
+    createBackground(xSquares, ySquares)
+    {
+        while (this.background.firstChild)
+        {
+            this.background.removeChild(this.background.lastChild);
+        }
+
+        const xSquareSize = Math.floor(window.innerWidth / xSquares);
+        const ySquareSize = Math.floor(window.innerHeight / ySquares);
+
+        for (let y = 0; y < ySquares; ++y)
+        {
+            for (let x = 0; x < xSquares; ++x)
+            {
+                const square = document.createElement("div");
+                square.classList.add("background-square");
+                square.style.width = xSquareSize + "px";
+                square.style.height = ySquareSize + "px";
+
+                const content = document.createElement("div");
+                content.classList.add("background-square-content");
+
+                square.appendChild(content);
+                this.background.appendChild(square);
+            }
+        }
+    }
+
+    createGround()
+    {
+        const ground = MeshBuilder.CreateGround("ground", {width: this.groundWidth, height: this.groundHeight}, this.scene);
+        ground.checkCollisions = true;
+
+        const groundMat = new StandardMaterial("groundMat");
+        groundMat.diffuseTexture = new Texture("assets/ground.jpg");
+        ground.material = groundMat;
     }
 }
 new App();
